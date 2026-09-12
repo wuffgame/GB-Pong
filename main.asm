2216297816223587386ef9b5b8f365b00a9d6199
wuff <kacper5555544444@gmail.com>
2026-09-12 14:31:42 +0200

    Added template for game

--- main.asm
+++ main.asm
@@ -1,1 +1,16 @@
 INCLUDE "hardware.inc"
+
+SECTION "Header", ROM0[$100]
+    jp EntryPoint
+    ds $150 - @, 0 ; Room for header
+
+EntryPoint:
+
+WaitVBlank:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank
+
+    ; Turn LCD off
+    ld a, 0
+    ld [rLCDC], a
